7ba63ea5e9113fda8f3b1c4fd2f45226895e493f
Lyle McClanahan <lmcclanahan@allbrandsservices.com>
2016-11-22 19:20:55 +0000

    Updated odoo9_vagrant_provisioning.sh

--- odoo9_vagrant_provisioning.sh
+++ odoo9_vagrant_provisioning.sh
@@ -2,6 +2,8 @@
 
 vagrant init ubuntu/trusty64
 vagrant up --provider virtualbox --no-parallel --destroy-on-error --install-provider
+mkdir extensions
+cd wget http://download.virtualbox.org/virtualbox/5.0.28/Oracle_VM_VirtualBox_Extension_Pack-5.0.28-111378.vbox-extpack -O ./extensions/oacle.vbox-extpack
 vagrant ssh
 sudo apt-get update;
 sudo apt-get install git curl wget vim mlocate -y
