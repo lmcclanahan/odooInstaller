de75c2f347c090bf0c502c2f4d41910c0e0815c0
Lyle McClanahan <lmcclanahan@allbrandsservices.com>
2016-11-22 18:55:04 +0000

    Updated odoo9_vagrant_provisioning.sh

--- odoo9_vagrant_provisioning.sh
+++ odoo9_vagrant_provisioning.sh
@@ -5,9 +5,8 @@ vagrant up --provider virtualbox --no-parallel --destroy-on-error --install-prov
 vagrant ssh
 sudo apt-get update;
 sudo apt-get install git curl wget vim mlocate -y
-wget https://raw.githubusercontent.com/Yenthe666/InstallScript/9.0/odoo_install.sh
-chmod +x odoo_install.sh
-./odoo_install.sh
+chmod +x install_odoo9.sh
+./install_odoo9.sh
  
 #  To make the odoo load from host pc have to edit Vagrantfile search and un-comment ( remove # from the beginning of the line ) 
 
